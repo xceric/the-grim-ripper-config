3894786da5c6be509f33de6737808f3e18517ebf
Eric Metzler <xceric@gmail.com>
2015-11-05 06:00:15 -0500

    added .abcde.conf to createlinks.sh

--- createlinks.sh
+++ createlinks.sh
@@ -5,3 +5,12 @@ do
   echo "Linking $file file..."
   ln -sf ~/repos/the-grim-ripper-config/applications/$file ~/.local/share/applications/$file
 done
+
+# link $HOME dot-files
+FILES=( .abcde.conf )
+
+for file in "${FILES[@]}"
+do
+  echo "Linking $file file..."
+  ln -sf ~/repos/the-grim-ripper-config/$file ~/$file
+done
