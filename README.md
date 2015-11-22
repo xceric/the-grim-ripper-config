9501f6848734a56d093a50aa2a89265b69b61be3
Eric Metzler <xceric@gmail.com>
2015-11-22 01:17:04 -0500

    incremental movement towards 15.10 and cover art

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Config files and dot-files for the grim ripper machine
 Got the ideas for adding cover art from: http://www.andrews-corner.org/getalbumart.html
 
 ogg-cover-art script came from: https://github.com/acabal/scripts/blob/master/ogg-cover-art
+
+This install works best with 15.10. abcde needed to be at >=2.7 and I don't mind having the latest for this machine.
--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@
 #
 
 # add HandBrakeCLI PPA
-apt-add-repository ppa:stebbins/handbrake-releases
+#apt-add-repository ppa:stebbins/handbrake-releases
 
 # add MakeMKV PPA
-apt-add-repository ppa:ubuntu-ebower/ebower
+apt-add-repository 'deb http://ppa.launchpad.net/ubuntu-ebower/ebower/ubuntu vivid main'
 
 apt-get update
 
@@ -17,11 +17,11 @@ apt-get update
 apt-get purge -y totem
 
 # install cd rip tools
-apt-get install -y abcde lame eyed3 flac oggenc vorbis-tools glyrc
+apt-get install -y abcde lame eyed3 flac oggenc vorbis-tools glyrc imagemagick
 wget https://raw.githubusercontent.com/acabal/scripts/master/ogg-cover-art -O ~/ogg-cover-art
 chmod +x ~/ogg-cover-art
 
 # install dvd/bd rip tools
-apt-get install -y HandBrake-GTK HandBrake-CLI libdvdread4 vobcopy makemkv-install libav-tools vlc
+apt-get install -y handbrake-gtk handbrake-cli libdvdread4 vobcopy makemkv-install libav-tools vlc
 /usr/share/doc/libdvdread4/install-css.sh
 makemkv-install latest
