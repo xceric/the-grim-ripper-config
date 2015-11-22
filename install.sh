3be89e5e40fd157bcd08347295810f2fc73a7021
Eric Metzler <xceric@gmail.com>
2015-11-22 10:58:12 -0500

    got mp3 import working, also updated install script with a couple packages that changed names in 15.10

--- install.sh
+++ install.sh
@@ -17,11 +17,11 @@ apt-get update
 apt-get purge -y totem
 
 # install cd rip tools
-apt-get install -y abcde lame eyed3 flac oggenc vorbis-tools glyrc imagemagick
+apt-get install -y abcde lame eyed3 flac vorbis-tools glyrc imagemagick
 wget https://raw.githubusercontent.com/acabal/scripts/master/ogg-cover-art -O ~/ogg-cover-art
 chmod +x ~/ogg-cover-art
 
 # install dvd/bd rip tools
-apt-get install -y handbrake-gtk handbrake-cli libdvdread4 vobcopy makemkv-install libav-tools vlc
+apt-get install -y handbrake handbrake-cli libdvdread4 vobcopy makemkv-install libav-tools-link vlc
 /usr/share/doc/libdvdread4/install-css.sh
 makemkv-install latest
