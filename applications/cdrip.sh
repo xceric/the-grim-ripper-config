d4d73505b5131a50353876de831267838b195ef7
Eric Metzler <xceric@gmail.com>
2015-11-29 13:15:05 -0500

    think I got things working as I want them for music, only doing ogg for now

--- applications/cdrip.sh
+++ applications/cdrip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 CDDRIVE="$(grep -o -G sr[01234] <<< $1)"              # GREP the drive from CLI arguments
-abcde -N -d /dev/$CDDRIVE                             # rip
+abcde -N -c ~/abcde-ogg.conf -d /dev/$CDDRIVE -x                            # rip
 sleep 60
 
