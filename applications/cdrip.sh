b123fe357171dbac7ef9c944b93a7878f357551d
Eric Metzler <xceric@gmail.com>
2016-12-06 20:41:52 -0500

    changed cdrip.sh to wait for user input to dismiss

--- applications/cdrip.sh
+++ applications/cdrip.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 CDDRIVE="$(grep -o -G sr[01234] <<< $1)"              # GREP the drive from CLI arguments
 abcde -N -c ~/abcde-ogg.conf -d /dev/$CDDRIVE -x                            # rip
-sleep 60
+
+echo "Complete! Press any key to dismiss..."
+read -n 1 -s
 
